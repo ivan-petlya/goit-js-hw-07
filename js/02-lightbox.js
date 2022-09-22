7cc6dcbf2c2cd4d1ea28657344d36ce1f8f8282b
ivan-petlya <104350431+ivan-petlya@users.noreply.github.com>
2022-09-22 12:46:07 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,11 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
 const galaryContainer = document.querySelector(".gallery");
 const gridImages = createImagesMarkup(galleryItems);
 galaryContainer.insertAdjacentHTML("beforeend", gridImages);
-// galaryContainer.addEventListener("click", onGridImagesClick);
 function createImagesMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
@@ -15,10 +13,7 @@ function createImagesMarkup(galleryItems) {
     })
     .join("");
 }
-// function onGridImagesClick(evt) {
-//   evt.preventDefault();
 let lightBoxGallery = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
-// }
